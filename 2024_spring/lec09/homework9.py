355c105261a098a2d77a3377f988012e64279380
xiaoyujun233 <xiaoyujunwork@gmail.com>
2024-07-16 06:00:41 +0900

    homework9

--- 2024_spring/lec09/homework9.py
+++ 2024_spring/lec09/homework9.py
@@ -1,20 +1,13 @@
 import numpy as np
 
 def fourier_synthesis(num_harmonics, X, T0):
-    '''
-    Use Fourier synthesis to resynthesize speech from its Fourier transform.
+    N = len(X)
+    x = np.zeros(N)
+    for l in range(1, num_harmonics + 1):
+        for n in range(N):
+            x[n] += np.abs(X[(l * N // T0) % N]) * np.cos(2 * np.pi * l * n / T0 + np.angle(X[(l * N // T0) % N]))
     
-    @param:
-    num_harmonics (scalar): the number of harmonics to resynthesize
-    X (np.ndarray(N)): a length-N Fourier transform
-    T0 (scalar): the pitch period, in samples
-        
-    @result:
-    x (np.ndarray(N)): a length-N waveform, resynthesized using Fourier synthesis
+    x *= 2 / N  # Scale by 2/N
     
-    The Fourier synthesis equation is this:
-    
-    x[n] = (2/N) * sum_{l=1}^{num_harmonics} |X[l*N//T0]| * cos(2*pi*l*n/T0 + angle(X[l*N//T0]))
-    '''
-    raise RuntimeError("You need to change this part")
+    return x
 
