eb6eaedf855712d50db730262f1295fcabc7b939
xiaoyujun233 <xiaoyujunwork@gmail.com>
2024-07-16 05:15:41 +0900

    homework3

--- 2024_spring/lec03/homework3.py
+++ 2024_spring/lec03/homework3.py
@@ -1,25 +1,26 @@
 
 
 def cancellation(list, stop_word):
-    '''
-    Copy elements one by one from input_list into output_list. 
-    If one of the elements is equal to the stop_word, then stop the function,
-    and return what you have so far.
-    '''
-    pass
+    output = []
+    for element in list:
+        if element == stop_word:
+            break
+        output.append(element)
+    return output
+
 
 def copy_all_but_skip_word(input_list, skip_word):
-    '''
-    This function should copy elements one by one from input_list into output_list.
-    If one of the elements is equal to the skip_word, then you should skip that element,
-    but keep checking all of the other elements.
-    '''
-    pass
+    output_list = []
+    for element in input_list:
+        if element == skip_word:
+            continue
+        output_list.append(element)
+    return output_list
+
 
 def my_average(input_list):
-    '''
-    You may assume that `input_list` is a non-empty list, in which every element is a number.  
-    Calculate the average value, and return it. 
-    '''
-    pass
+    total = sum(input_list)
+    count = len(input_list)
+    average = total / count
+    return average
 
