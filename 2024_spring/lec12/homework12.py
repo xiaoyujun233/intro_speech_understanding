347a4c57b607b1dea63a65ace608c24d2f1b5018
xiaoyujun233 <xiaoyujunwork@gmail.com>
2024-07-16 06:29:50 +0900

    homework12

--- 2024_spring/lec12/homework12.py
+++ 2024_spring/lec12/homework12.py
@@ -1,13 +1,6 @@
-import gtts
+from gtts import gTTS
 
 def synthesize(text, lang, filename):
-    '''
-    Use gtts.gTTs(text=text, lang=lang) to synthesize speech, then write it to filename.
+    tts = gTTS(text=text, lang=lang)
     
-    @params:
-    text (str) - the text you want to synthesize
-    lang (str) - the language in which you want to synthesize it
-    filename (str) - the filename in which it should be saved
-    '''
-    raise RuntimeError("FAIL! You need to change this function so that it works!")
-
+    tts.save(filename)
