5bdfff10074a24043d55d20f1128b7bf10e3959a
xiaoyujun233 <xiaoyujunwork@gmail.com>
2024-07-16 06:25:25 +0900

    homework10

--- 2024_spring/lec10/homework10.py
+++ 2024_spring/lec10/homework10.py
@@ -1,51 +1,27 @@
 import numpy as np
 
 def waveform_to_frames(waveform, frame_length, step):
-    '''
-    Chop a waveform into overlapping frames.
+    num_frames = int((len(waveform) - frame_length) / step) + 1
+    frames = np.zeros((frame_length, num_frames))
     
-    @params:
-    waveform (np.ndarray(N)) - the waveform
-    frame_length (scalar) - length of the frame, in samples
-    step (scalar) - step size, in samples
+    for t in range(num_frames):
+        frames[:, t] = waveform[t * step : t * step + frame_length]
     
-    @returns:
-    frames (np.ndarray((frame_length, num_frames))) - waveform chopped into frames
-    
-    num_frames should be at least int((len(speech)-frame_length)/step); it may be longer.
-    For every n and t such that 0 <= t*step+n <= N-1, it should be the case that 
-       frames[n,t] = waveform[t*step+n]
-    '''
-    raise RuntimeError("You need to change this part")
+    return frames
 
 def frames_to_stft(frames):
-    '''
-    Take the FFT of every column of the frames matrix.
+    frame_length, num_frames = frames.shape
+    stft = np.zeros((frame_length, num_frames), dtype=complex)
     
-    @params:
-    frames (np.ndarray((frame_length, num_frames))) - the speech samples (real-valued)
+    for t in range(num_frames):
+        stft[:, t] = np.fft.fft(frames[:, t])
     
-    @returns:
-    stft (np.ndarray((frame_length,num_frames))) - the STFT (complex-valued)
-    '''
-    raise RuntimeError("You need to change this part")
+    return stft
 
 def stft_to_spectrogram(stft):
-    '''
-    Calculate the level, in decibels, of each complex-valued sample of the STFT,
-    normalized so the highest value is 0dB, 
-    and clipped so that the lowest value is -60dB.
-    
-    @params:
-    stft (np.ndarray((frame_length,num_frames))) - STFT (complex-valued)
-    
-    @returns:
-    spectrogram (np.ndarray((frame_length,num_frames)) - spectrogram (real-valued)
-    
-    The spectrogram should be expressed in decibels (20*log10(abs(stft)).
-    np.amax(spectrogram) should be 0dB.
-    np.amin(spectrogram) should be no smaller than -60dB.
-    '''
-    raise RuntimeError("You need to change this part")
+    abs_stft = np.abs(stft)
+    spectrogram = 20 * np.log10(np.maximum(0.001, abs_stft/np.amax(abs_stft)))
+
+    return spectrogram
 
 
